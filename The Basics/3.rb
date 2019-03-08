2c4e28a7fbef4c5536ab6d7dda4a1de599f61f5f
Sunny Chan <cytsunny@gmail.com>
2019-03-08 04:17:13 +0000

    Answer code for the first chapter

---
+++ The Basics/3.rb
@@ -0,0 +1,11 @@
+movies = { :space_battleship_yamato => 1977,
+            :galaxy_express_999 => 1979,
+        	  :porco_rosso => 1992,
+            :howl_s_moving_castle => 2004,
+            :the_wind_rises => 2013}
+
+puts movies[:space_battleship_yamato]
+puts movies[:galaxy_express_999 ]
+puts movies[:porco_rosso]
+puts movies[:howl_s_moving_castle]
+puts movies[:the_wind_rises]
---
+++ The Basics/4.rb
@@ -0,0 +1,7 @@
+dates = [1977, 1979, 1992, 2004, 2013]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
